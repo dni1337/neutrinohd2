eb012a6cc3f230255c04c7998fe68e752909aaed
dni1337 <15782082+dni1337@users.noreply.github.com>
2019-06-13 15:29:06 +0200

    Update autogen.sh

--- plugins/autogen.sh
+++ plugins/autogen.sh
@@ -26,14 +26,14 @@ DIE=0
 	DIE=1
 }
 
-(libtool --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have libtool installed to compile $package."
-	echo "Download the appropriate package for your system,"
-	echo "or get the source from one of the GNU ftp sites"
-	echo "listed in http://www.gnu.org/order/ftp.html"
-	DIE=1
-}
+#(libtool --version) < /dev/null > /dev/null 2>&1 || {
+#	echo
+#	echo "You must have libtool installed to compile $package."
+#	echo "Download the appropriate package for your system,"
+#	echo "or get the source from one of the GNU ftp sites"
+#	echo "listed in http://www.gnu.org/order/ftp.html"
+#	DIE=1
+#}
 
 if test "$DIE" -eq 1; then
 	exit 1
